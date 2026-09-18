9f5584ccb6a287ffc9039e2909773b011f0d45af
imad tbn <tabaniimadeddine@gmail.com>
2026-09-18 12:28:02 +0100

    Connect doctor submissions to deployed Google Sheets intake

--- js/submission-config.js
+++ js/submission-config.js
@@ -1,4 +1,4 @@
 // Paste the deployed Google Apps Script Web App URL below after following
 // tools/google-apps-script/README.md. Keep the review spreadsheet private.
-export const SUBMISSION_ENDPOINT='';
+export const SUBMISSION_ENDPOINT='https://script.google.com/macros/s/AKfycbwCONiva41cX_2t1BA_hqkZ0PreJXOqeGplZuZtHKp_dTIHHun6BbtSUJ-3-DfYoIm7/exec';
 export const SUBMISSION_VERSION='tabibi-v1';
