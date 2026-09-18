24f908a5aaf4d33bbc7a77a883e30ef930a46544
imad tbn <tabaniimadeddine@gmail.com>
2026-09-18 18:22:53 +0100

    Update SUBMISSION_ENDPOINT URL

--- js/submission-config.js
+++ js/submission-config.js
@@ -1,4 +1,4 @@
 // Paste the deployed Google Apps Script Web App URL below after following
 // tools/google-apps-script/README.md. Keep the review spreadsheet private.
-export const SUBMISSION_ENDPOINT='https://script.google.com/macros/s/AKfycbwCONiva41cX_2t1BA_hqkZ0PreJXOqeGplZuZtHKp_dTIHHun6BbtSUJ-3-DfYoIm7/exec';
+export const SUBMISSION_ENDPOINT='https://script.google.com/macros/s/AKfycbzOdNcEScmsHsGLpPAUmlXjGhYW4kIvyjgiLpgStwq6mBv1oXiGSbYvgg222R-DvT8T/exec';
 export const SUBMISSION_VERSION='tabibi-v1';
